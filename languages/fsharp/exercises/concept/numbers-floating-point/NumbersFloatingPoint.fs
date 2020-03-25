workingjubilee/exercism-v3

[F#] Add numbers-floating-point exercise

languages/fsharp/exercises/concept/numbers-floating-point/NumbersFloatingPoint.fs
@@ -1,0 +1,10 @@
+module NumbersFloatingPoint
+
+let annualPercentageYield (balance: decimal): float =
+    failwith "Please implement the 'annualPercentageYield' function"
+    
+let annualBalanceUpdate (balance: decimal): decimal =
+    failwith "Please implement the 'annualBalanceUpdate' function"
+    
+let yearsBeforeDesiredBalance (balance: decimal) (targetBalance: decimal): int =
+    failwith "Please implement the 'yearsBeforeDesiredBalance' function" 

oanaOM/v3

languages/fsharp/exercises/concept/numbers-floating-point/NumbersFloatingPoint.fs
@@ -1,0 +1,10 @@
+module NumbersFloatingPoint
+
+let interestRate (balance: decimal): single =
+    failwith "Please implement the 'interestRate' function"
+
+let annualBalanceUpdate (balance: decimal): decimal =
+    failwith "Please implement the 'annualBalanceUpdate' function"
+
+let amountToDonate (balance: decimal) (taxFreePercentage: double): int =
+    failwith "Please implement the 'amountToDonate' function"